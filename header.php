7ec04de4af7409eb796bf21fe674305ed1263e38
Ross Salanoa <ross.salanoa@dmail.dixie.edu>
2017-10-16 01:50:39 -0600

    fixing header

--- header.php
+++ header.php
@@ -12,16 +12,16 @@
 </head>
 <body>
      <header>
+          <div class="container">
                <nav>
-				<div class="container">
                     <div class="logo">
                          <h2>Flat Earth</h2>
                     </div>
                     <ul id="nav-ul">
                          <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
                     </ul>
-          		</div>
-			</nav>
+               </nav>
+          </div>
      </header>
      <div class="container">
           <div id="intro" class="row">
