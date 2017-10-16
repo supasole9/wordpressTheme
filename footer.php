183941ab42eef6fa74c5afe452c7b4ca86347c1c
Ross Salanoa <ross.salanoa@dmail.dixie.edu>
2017-10-16 01:42:03 -0600

    fixing footer

--- footer.php
+++ footer.php
@@ -1,9 +1,16 @@
 <footer>
      <div class="container">
           <div class="row">
-               <div class="col4l">address</div>
-               <div class="col4l">tel</div>
-               <div class="col4l">email</div>
+               <div class="col4l col12s foot-ctr">
+                    500 S 800 E
+                    St George UT 84770
+               </div>
+               <div class="col4l col12s foot-ctr">
+                    435.770.0552
+               </div>
+               <div class="col4l col12s foot-ctr">
+                    flatearth@flatearth.org
+               </div>
           </div>
      </div>
 </footer>
