061e159406ec9729500ca35c91b0bca9cda75954
Ross Salanoa <ross.salanoa@dmail.dixie.edu>
2017-10-16 01:43:48 -0600

    fixing footer

--- footer.php
+++ footer.php
@@ -2,8 +2,8 @@
      <div class="container">
           <div class="row">
                <div class="col4l col12s foot-ctr">
-                    500 S 800 E
-                    St George UT 84770
+                    500 S 800 E <br>
+                    St George UT 84770 
                </div>
                <div class="col4l col12s foot-ctr">
                     435.770.0552
