7f9e76e56b404073a65926eea1805cafd78f88df
Ross Salanoa <ross.salanoa@dmail.dixie.edu>
2017-10-16 01:12:17 -0600

    fixing header

--- index.php
+++ index.php
@@ -28,20 +28,5 @@
 	<?php endif; ?>
 </div><!--.content-->
 </div>
-
-<div id="liveshow">
-     <div class="container">
-          <div class="row">
-               <div class="col6l col12x">
-                    <h1>Watch the live show</h1>
-                    <p>You know you want to know more</p>
-                    <p>This will help you to start thinking out of the box and formulate your own ideas.</p>
-               </div>
-               <div class="col6l col12s topmargin">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/wRnQuJTLt5o?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
-               </div>
-          </div>
-     </div>
-</div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
